0d1e4bcafcab74fa71fde7ea00e99620ed322422
hwang67120-bit <hwang67120@gmail.com>
2026-07-16 11:45:21 +0900

    PointChargeService.java

--- src/main/java/com/example/kservertask/point/service/PointChargeService.java
+++ src/main/java/com/example/kservertask/point/service/PointChargeService.java
@@ -29,6 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PointChargeService {
 
     private final AppUserRepository appUserRepository;
